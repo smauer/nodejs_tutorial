ba3d4b8b4d68c6f9810dd4acd930c1129d5a1afa
Sam Mauer <sam.mauer@gmail.com>
2017-09-04 16:23:24 -0500

    Finished Episode 7 - bullets and collisions

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ var Entity = function() {
         self.x += self.spdX;
         self.y += self.spdY;
     }
+
+    self.getDistance = function(pt) {
+        return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+    }
+
     return self;
 }
 
@@ -38,12 +43,25 @@ var Player = function(id) {
     self.pressingUp = false;
     self.pressingLeft = false;
     self.pressingRight = false;
+    self.pressingAttack = false;
+    self.mouseAngle = 0;
     self.maxSpd = 10;
 
     var super_update = self.update;
     self.update = function() {
         self.updateSpd();
         super_update();
+
+        if (self.pressingAttack)
+        {
+            self.shootBullet(self.mouseAngle);
+        }
+    }
+
+    self.shootBullet = function(angle) {
+        var b = Bullet(self.id, angle);
+        b.x = self.x;
+        b.y = self.y;
     }
 
     self.updateSpd = function(){
@@ -86,6 +104,12 @@ Player.onConnect = function(socket) {
             case 'down':
                 player.pressingDown = data.state;
                 break;
+            case 'attack':
+                player.pressingAttack = data.state;
+                break;
+            case 'mouseAngle':
+                player.mouseAngle = data.state;
+                break;
             default:
                 break;
         }
@@ -106,12 +130,12 @@ Player.update = function() {
     return pack;
 }
 
-var Bullet = function(angle){
+var Bullet = function(parent, angle){
     var self = Entity();
     self.id = Math.random();
     self.spdX = Math.cos(angle/180*Math.PI) * 10;
     self.spdY = Math.sin(angle/180*Math.PI) * 10;
-
+    self.parent = parent;
     self.timer = 0;
     self.toRemove = false;
 
@@ -120,6 +144,13 @@ var Bullet = function(angle){
         if (self.timer++ > 100)
             self.toRemove = true;
         super_update();
+
+        for(var i in Player.list){
+            var p = Player.list[i];
+            if (self.getDistance(p) < 32 && self.parent !== p.id) {
+                self.toRemove = true;
+            }
+        }
     }
 
     Bullet.list[self.id] = self;
@@ -128,17 +159,19 @@ var Bullet = function(angle){
 Bullet.list = {};
 
 Bullet.update = function() {
-    if (Math.random() < 0.1)
-        Bullet(Math.random() * 360);
-
     var pack = [];
     for (var i in Bullet.list) {
         var bullet = Bullet.list[i];
         bullet.update();
-        pack.push({
-            x:bullet.x,
-            y:bullet.y
-        });
+        if(bullet.toRemove){
+            delete Bullet.list[i];
+        }
+        else{
+            pack.push({
+                x:bullet.x,
+                y:bullet.y
+            });
+        }
     }
     return pack;
 }
